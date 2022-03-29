135f3c2ff98d3bc290572f8f18aa6fa1f905c535
mcpeblocker <mcpeblocker@gmail.com>
2022-03-29 14:45:48 +0500

    api setup for all books

--- src/Pages/Books.jsx
+++ src/Pages/Books.jsx
@@ -1,49 +1,48 @@
 import { LanguageContext } from "../LanguageContext";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import IconBtn from "../Components/IconBtn";
 import { Box } from "@mui/system";
 import BookMini from "../Components/BooksMini";
-import Poisk from "../Icons/poisk.svg"
+import Poisk from "../Icons/poisk.svg";
 import { useEffect, useState } from "react";
 import api from "../api";
 
-export default function Books(){
+export default function Books() {
+  const [books, setBooks] = useState([]);
 
-    const [books, setBooks] = useState([]);
-
-    useEffect(async () => {
-        const response = await api.get('/books');
-        console.log(response);
-    }, []);
-
-    return(
-        <LanguageContext.Consumer>
-            {lang=>(
-                <div className="books-page" >
-                    <Link to="/"> <Box component="div" className="back-icon" style={{top:"24px"}} id="back-icon-link" >
-                        <IconBtn />
-                    </Box></Link>
-                    <span className="books-header">{lang.kitoblar}</span>
-                    <Link to="/" className="Mylink" >
-                        <img src={Poisk} alt="" className="search-icon"/>
-                    </Link>
-                    <div className="for-scroll" style={{marginTop:"44px"}}>
-                        <BookMini Name={lang.book1} Author={lang.book12} />
-                        <BookMini Name={lang.book1} Author={lang.book12} />
-                        <BookMini Name={lang.book1} Author={lang.book12} />
-                        <BookMini Name={lang.book1} Author={lang.book12} />
-                        <BookMini Name={lang.book1} Author={lang.book12} />
-                        <BookMini Name={lang.book1} Author={lang.book12} />
-                        <BookMini Name={lang.book1} Author={lang.book12} />
-                        <BookMini Name={lang.book1} Author={lang.book12} />
-                        <BookMini Name={lang.book1} Author={lang.book12} />
-                        <BookMini Name={lang.book1} Author={lang.book12} />
-                        <BookMini Name={lang.book1} Author={lang.book12} />
-                        <BookMini Name={lang.book1} Author={lang.book12} />
-                        <BookMini Name={lang.book1} Author={lang.book12} />
-                    </div>
-                </div>
-            )}
-        </LanguageContext.Consumer>
-    )
+  useEffect(async () => {
+    const response = await api.get("/books");
+    if (response.status === 200) {
+      setBooks(response.data);
+      console.log(response);
+    }
+  }, []);
+  return (
+    <LanguageContext.Consumer>
+      {(lang) => (
+        <div className="books-page">
+          <Link to="/">
+            {" "}
+            <Box
+              component="div"
+              className="back-icon"
+              style={{ top: "24px" }}
+              id="back-icon-link"
+            >
+              <IconBtn />
+            </Box>
+          </Link>
+          <span className="books-header">{lang.kitoblar}</span>
+          <Link to="/" className="Mylink">
+            <img src={Poisk} alt="" className="search-icon" />
+          </Link>
+          <div className="for-scroll" style={{ marginTop: "44px" }}>
+            {books.map((book) => (
+              <BookMini key={book.bookId} book={book} />
+            ))}
+          </div>
+        </div>
+      )}
+    </LanguageContext.Consumer>
+  );
 }
