67a5214f40c0c176730f5f45bc2d2c70ce5a208d
mcpeblocker <mcpeblocker@gmail.com>
2022-03-29 20:58:14 +0500

    cors

--- src/api.js
+++ src/api.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://api.library.mcpeblocker.uz/api'
+    baseURL: 'http://api.library.mcpeblocker.uz/api',
+    mode: 'no-cors',
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        Accept: 'application/json',
+        "Content-Type": "application/json"
+    }
 });
 
 export default api;
